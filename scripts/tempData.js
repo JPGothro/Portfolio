f7fca230c0788fd40a0514f0486c5ce92cf010e6
JPGothro <jpgothro@gmail.com>
2016-09-20 17:40:32 -0700

    restructured code, added scripts, data

---
+++ scripts/tempData.js
@@ -0,0 +1,20 @@
+var localData = [
+  {projectTitle: 'YachtZoe!',
+   projectSummary: 'A dice game',
+   projectDetails: 'Built as a Final project for Code Fellows 201 class.',
+   projectLink: 'https://jpgothro.github.io/yachtzoe_project/index.html',
+   projectSrcLink: ''
+  },
+  {projectTitle: 'Fake project',
+   projectSummary: 'Well, it is not real',
+   projectDetails: 'You can make up anything for this one!',
+   projectLink: 'http://www.telegraph.co.uk/news/2016/09/19/cuddling-kittens-can-kill-you-warn-scientists/',
+   projectSrcLink: 'http://catadoptionteam.org/'
+ },
+ {projectTitle: 'Third Partial project',
+  projectSummary: 'Project incomplete',
+  projectDetails: 'Incomplete, so no details available at this time.',
+  projectLink: null,
+  projectSrcLink: null
+ }
+];
